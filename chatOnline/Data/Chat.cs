db291b0a50fabc99d795479ec2e6b33af362a3e9
user <user@DESKTOP-RA6T6M6>
2024-05-01 22:39:34 +0300

    добавлены: Chat, Messages, DataContext, UserContext; чуть-чуть изменен Chatik; исправлены некоторые ошибки в Registr и Voiti , появились новые

---
+++ chatOnline/Data/Chat.cs
@@ -0,0 +1,13 @@
+﻿namespace chatOnline.Data
+{
+    public class Chat
+    {
+        public string Id { get; private set; }
+        public List<Message> Messages { get; private set; }
+
+        public void SendMessage(Message m)
+        {
+            Messages.Add(m);
+        }
+    }
+}
